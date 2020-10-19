b3b95bb30d8124d15cf76690aea35282f29c1534
Mr Hien <nvhien.d13ptit@gmail.com>
2020-10-19 10:22:17 +0700

    update sierra filter

--- LFLiveKit/filter/GPUImageSierraFilter.m
+++ LFLiveKit/filter/GPUImageSierraFilter.m
@@ -3,15 +3,33 @@
 #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
 NSString *const kGPUImageSierraFragmentShaderString = SHADER_STRING
                                                        (
-    varying highp vec2 textureCoordinate;
+                                                        varying mediump vec2 textureCoordinate;
 
-    uniform sampler2D inputImageTexture;
+                                                        uniform samplerExternalOES inputImageTexture;
+                                                        uniform sampler2D inputImageTexture2; //blowout;
+                                                        uniform sampler2D inputImageTexture3; //overlay;
+                                                        uniform sampler2D inputImageTexture4; //map
+                                                        uniform float strength;
 
-    void main(){
-    lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+                                                        void main()
+                                                        {
+                                                            vec4 originColor = texture2D(inputImageTexture, textureCoordinate);
+                                                            vec4 texel = texture2D(inputImageTexture, textureCoordinate);
+                                                            vec3 bbTexel = texture2D(inputImageTexture2, textureCoordinate).rgb;
 
-    gl_FragColor = vec4((textureColor.rgb), textureColor.w);
-}
+                                                            texel.r = texture2D(inputImageTexture3, vec2(bbTexel.r, texel.r)).r;
+                                                            texel.g = texture2D(inputImageTexture3, vec2(bbTexel.g, texel.g)).g;
+                                                            texel.b = texture2D(inputImageTexture3, vec2(bbTexel.b, texel.b)).b;
+
+                                                            vec4 mapped;
+                                                            mapped.r = texture2D(inputImageTexture4, vec2(texel.r, .16666)).r;
+                                                            mapped.g = texture2D(inputImageTexture4, vec2(texel.g, .5)).g;
+                                                            mapped.b = texture2D(inputImageTexture4, vec2(texel.b, .83333)).b;
+                                                            mapped.a = 1.0;
+
+                                                            mapped.rgb = mix(originColor.rgb, mapped.rgb, strength);
+                                                            gl_FragColor = mapped;
+                                                        }
 
                                                        );
 #else
