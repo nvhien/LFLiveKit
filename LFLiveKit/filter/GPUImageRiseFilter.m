fea7c3dd4e767a5447fa699c6d5170e3f4bc7a3f
Mr Hien <nvhien.d13ptit@gmail.com>
2020-10-20 15:04:35 +0700

    add rise filter

--- LFLiveKit/filter/GPUImageRiseFilter.m
+++ LFLiveKit/filter/GPUImageRiseFilter.m
@@ -3,29 +3,73 @@
 #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
 NSString *const kGPUImageRiseFragmentShaderString = SHADER_STRING
                                                        (
-    varying highp vec2 textureCoordinate;
+                                                        precision highp float;
 
-    uniform sampler2D inputImageTexture;
+                                                        varying highp vec2 textureCoordinate;
 
-    void main(){
-    lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+                                                        uniform sampler2D inputImageTexture;
+                                                        uniform sampler2D inputImageTexture2; //blowout;
+                                                        uniform sampler2D inputImageTexture3; //overlay;
+                                                        uniform sampler2D inputImageTexture4; //map
 
-    gl_FragColor = vec4((textureColor.rgb), textureColor.w);
-}
+                                                        uniform float strength;
+
+                                                        void main()
+                                                        {
+                                                            vec4 originColor = texture2D(inputImageTexture, textureCoordinate);
+                                                            vec4 texel = texture2D(inputImageTexture, textureCoordinate);
+                                                            vec3 bbTexel = texture2D(inputImageTexture2, textureCoordinate).rgb;
+
+                                                            texel.r = texture2D(inputImageTexture3, vec2(bbTexel.r, texel.r)).r;
+                                                            texel.g = texture2D(inputImageTexture3, vec2(bbTexel.g, texel.g)).g;
+                                                            texel.b = texture2D(inputImageTexture3, vec2(bbTexel.b, texel.b)).b;
+
+                                                            vec4 mapped;
+                                                            mapped.r = texture2D(inputImageTexture4, vec2(texel.r, .16666)).r;
+                                                            mapped.g = texture2D(inputImageTexture4, vec2(texel.g, .5)).g;
+                                                            mapped.b = texture2D(inputImageTexture4, vec2(texel.b, .83333)).b;
+                                                            mapped.a = 1.0;
+
+                                                            mapped.rgb = mix(originColor.rgb, mapped.rgb, strength);
+
+                                                            gl_FragColor = mapped;
+                                                        }
 
                                                        );
 #else
 NSString *const kGPUImageRiseFragmentShaderString = SHADER_STRING
                                                       (
-    varying vec2 textureCoordinate;
+                                                       precision mediump float;
 
-    uniform sampler2D inputImageTexture;
+                                                       varying mediump vec2 textureCoordinate;
 
-    void main(){
-    vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+                                                       uniform samplerExternalOES inputImageTexture;
+                                                       uniform sampler2D inputImageTexture2; //blowout;
+                                                       uniform sampler2D inputImageTexture3; //overlay;
+                                                       uniform sampler2D inputImageTexture4; //map
 
-    gl_FragColor = vec4((textureColor.rgb), textureColor.w);
-}
+                                                       uniform float strength;
+
+                                                       void main()
+                                                       {
+                                                           vec4 originColor = texture2D(inputImageTexture, textureCoordinate);
+                                                           vec4 texel = texture2D(inputImageTexture, textureCoordinate);
+                                                           vec3 bbTexel = texture2D(inputImageTexture2, textureCoordinate).rgb;
+
+                                                           texel.r = texture2D(inputImageTexture3, vec2(bbTexel.r, texel.r)).r;
+                                                           texel.g = texture2D(inputImageTexture3, vec2(bbTexel.g, texel.g)).g;
+                                                           texel.b = texture2D(inputImageTexture3, vec2(bbTexel.b, texel.b)).b;
+
+                                                           vec4 mapped;
+                                                           mapped.r = texture2D(inputImageTexture4, vec2(texel.r, .16666)).r;
+                                                           mapped.g = texture2D(inputImageTexture4, vec2(texel.g, .5)).g;
+                                                           mapped.b = texture2D(inputImageTexture4, vec2(texel.b, .83333)).b;
+                                                           mapped.a = 1.0;
+
+                                                           mapped.rgb = mix(originColor.rgb, mapped.rgb, strength);
+
+                                                           gl_FragColor = mapped;
+                                                       }
 
                                                       );
 #endif
@@ -38,6 +82,8 @@ NSString *const kGPUImageRiseFragmentShaderString = SHADER_STRING
         return nil;
     }
 
+    [self setFloat:1.0 forUniformName:@"strength"];
+    
     return self;
 }
 
